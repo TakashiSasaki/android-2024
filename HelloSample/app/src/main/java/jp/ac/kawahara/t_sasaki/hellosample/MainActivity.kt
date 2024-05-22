26b39dd0c276edc5d4bd0af81ab0e0efd2091437
Takashi SASAKI <takashi316@gmail.com>
2024-05-22 13:06:37 +0900

    add HelloSample

---
+++ HelloSample/app/src/main/java/jp/ac/kawahara/t_sasaki/hellosample/MainActivity.kt
@@ -0,0 +1,36 @@
+package jp.ac.kawahara.t_sasaki.hellosample
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListener()
+        btClick.setOnClickListener( listener )
+
+        //上の三行と以下の一行は等価
+        //findViewById<Button>(R.id.btClick).setOnClickListener(HelloListener())
+
+    }//onCreate
+
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(v: View) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+            val inputStr : String = input.text.toString()
+            output.text = inputStr + "さん、こんにちは！"
+        }//onClick
+
+    }//HelloListener
+
+}//class MainActivity
+
