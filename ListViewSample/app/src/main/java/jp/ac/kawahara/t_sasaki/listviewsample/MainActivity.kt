1d73dbadff70089d60f8fe96f458590114bbe223
Takashi SASAKI <takashi316@gmail.com>
2024-06-04 10:53:43 +0900

    ListViewSample toast

--- ListViewSample/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample/MainActivity.kt
+++ ListViewSample/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample/MainActivity.kt
@@ -1,6 +1,9 @@
 package jp.ac.kawahara.t_sasaki.listviewsample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }//onCreate
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick
+                    (parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item  = parent.getItemAtPosition(position) as String
+            val show = "あなたが選んだ定食：$item"
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG)
+        }
+
     }
+
 }
