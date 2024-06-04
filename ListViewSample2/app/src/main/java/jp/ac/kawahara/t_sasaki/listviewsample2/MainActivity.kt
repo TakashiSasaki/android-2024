9e5bbf24398072b426d7ad89d3b168dae0fbb2ea
Takashi SASAKI <takashi316@gmail.com>
2024-06-04 12:30:42 +0900

    menuList

--- ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.ac.kawahara.t_sasaki.listviewsample2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        var menuList = mutableListOf("から揚げ定食", "ハンバーグ定食", "生姜焼き定食",
+            "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食",
+            "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "八宝菜定食", "酢豚定食",
+            "豚の角煮", "焼き鳥", "焼き魚定食", "焼肉定食")
+
     }
 }
