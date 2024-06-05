f079077097543ada1b5fc9d91dfe3f336f168909
Takashi SASAKI <takashi316@gmail.com>
2024-06-05 14:35:13 +0900

    onCreate

--- ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/MainActivity.kt
@@ -2,10 +2,8 @@ package jp.ac.kawahara.t_sasaki.listviewsample2
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import androidx.activity.enableEdgeToEdge
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +15,17 @@ class MainActivity : AppCompatActivity() {
             "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "八宝菜定食", "酢豚定食",
             "豚の角煮", "焼き鳥", "焼き魚定食", "焼肉定食")
 
-    }
-}
+        // 以下のような書き方でも可
+        //var x = ArrayList<String>()
+        //x.add("から揚げ定食")
+        //x.add("ハンバーグ定食")
+
+        var adapter = ArrayAdapter(this@MainActivity,
+            android.R.layout.simple_list_item_1,
+            menuList)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.adapter = adapter
+
+    }//onCreate
+}//MainActivity
