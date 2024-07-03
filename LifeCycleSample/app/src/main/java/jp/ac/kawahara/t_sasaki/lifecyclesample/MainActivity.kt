911f954c4dfd6a226edccb5d9fe67234d29cc866
Takashi SASAKI <takashi316@gmail.com>
2024-07-03 15:22:40 +0900

    LifeCycleSample completed

--- LifeCycleSample/app/src/main/java/jp/ac/kawahara/t_sasaki/lifecyclesample/MainActivity.kt
+++ LifeCycleSample/app/src/main/java/jp/ac/kawahara/t_sasaki/lifecyclesample/MainActivity.kt
@@ -1,14 +1,51 @@
 package jp.ac.kawahara.t_sasaki.lifecyclesample
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+    }// onCreate
+
+    override fun onStart() {
+        Log.i("LifeCycleSample", "Main onStart() called.")
+        super.onStart()
+    }// onStart
+
+    override fun onRestart() {
+        Log.i("LifeCycleSample", "Main onRestart() called.")
+        super.onRestart()
+    }// onRestart
+
+    override fun onResume() {
+        Log.i("LifeCycleSample", "Main onResume() called.")
+        super.onResume()
+    }// onResume
+
+    override fun onPause() {
+        Log.i("LifeCycleSample", "Main onPause() called.")
+        super.onPause()
+    }// onPause
+
+    override fun onStop() {
+        Log.i("LifeCycleSample", "Main onStop() called.")
+        super.onStop()
+    }// onStop
+
+    override fun onDestroy() {
+        Log.i("LifeCycleSample", "Main onDestroy() called.")
+        super.onDestroy()
+    }// onDestroy
+
+    fun onButtonClick(v: View){
+        val intent = Intent(this@MainActivity, SubActivity::class.java)
+        startActivity(intent)
+    }// onButtonClick
+
 }
--- LifeCycleSample/app/src/main/java/jp/ac/kawahara/t_sasaki/lifecyclesample/SubActivity.kt
+++ LifeCycleSample/app/src/main/java/jp/ac/kawahara/t_sasaki/lifecyclesample/SubActivity.kt
@@ -1,6 +1,8 @@
 package jp.ac.kawahara.t_sasaki.lifecyclesample
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +10,42 @@ import androidx.core.view.WindowInsetsCompat
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Sub onCreate() called.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+    }
+
+    override fun onStart() {
+        Log.i("LifeCycleSample", "Sub onStart() called.")
+        super.onStart()
+    }// onStart
+
+    override fun onRestart() {
+        Log.i("LifeCycleSample", "Sub onRestart() called.")
+        super.onRestart()
+    }// onRestart
+
+    override fun onResume() {
+        Log.i("LifeCycleSample", "Sub onResume() called.")
+        super.onResume()
+    }// onResume
+
+    override fun onPause() {
+        Log.i("LifeCycleSample", "Sub onPause() called.")
+        super.onPause()
+    }// onPause
+
+    override fun onStop() {
+        Log.i("LifeCycleSample", "Sub onStop() called.")
+        super.onStop()
+    }// onStop
+
+    override fun onDestroy() {
+        Log.i("LifeCycleSample", "Sub onDestroy() called.")
+        super.onDestroy()
+    }// onDestroy
+
+    fun onButtonClick(v: View){
+        finish()
     }
 }
