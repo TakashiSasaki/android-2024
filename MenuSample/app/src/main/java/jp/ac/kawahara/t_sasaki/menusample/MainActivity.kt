b25639d9ad4378a8215139491c8ae5dc121d0968
Takashi SASAKI <takashi316@gmail.com>
2024-07-09 11:15:20 +0900

    teishoku

--- MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,15 +14,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private var _menuList : MutableList<MutableMap<String, Any>> = mutableListOf()
-
-
+    private val _from = arrayOf("name", "price")
+    private val _to  = intArrayOf(R.id.tvMenuNameRow, R.id.tvMenuPriceRow)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _menuList = createTeishokuList()
+        val adapter = SimpleAdapter(this@MainActivity,
+            _menuList,
+            R.layout.row,
+            _from, _to)
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.adapter = adapter
+        lvMenu.onItemClickListener = ListItemClickListener()
     }// onCreate
 
-    private fun createTeishokuList(){
+    private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
         var menuList : MutableList<MutableMap<String, Any>> = mutableListOf()
         menuList.add(mutableMapOf("name" to "唐揚げ定食",
             "price" to 800,
@@ -28,21 +38,22 @@ class MainActivity : AppCompatActivity() {
         menuList.add(mutableMapOf("name" to "ハンバーク定食",
             "price" to 850,
             "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。"))
-
+        return menuList
     }
     private inner class ListItemClickListener : AdapterView.OnItemClickListener {
         override fun onItemClick(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
             val item = parent.getItemAtPosition(position)
-                    as MutableMap<String, Any>
+                            as MutableMap<String, Any>
             val menuName = item["name"] as String
             val menuPrice = item["price"] as Int
 
             val intent2MenuThanks = Intent(this@MainActivity,
                 MenuThanksActivity::class.java)
             intent2MenuThanks.putExtra("menuName", menuName)
-            intent2MenuThanks.putExtra("menuPrice", "${menuPrice}円")
+            intent2MenuThanks.putExtra("menuPrice",
+                "${menuPrice}円")
             startActivity(intent2MenuThanks)
-        }
+        }// onItemClick
 
-    }
-}
+    } // ListItemClickListener
+} // MainActivity
